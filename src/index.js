495888bb085bbda2e8d79a8772a92214431114e9
LUIGI LORIA s297070 <s297070@studenti.polito.it>
2022-05-27 21:53:24 +0200

    fix: logo icon on mobile does not stretch anymore
    Resolve #16

--- src/index.js
+++ src/index.js
@@ -217,7 +217,7 @@ const App = () => {
   return html`
     <div class="p-4">
       <div class="flex align-center mb-4">
-        <img class="w-10 sm:w-20 mr-3" src=${svgLogo} alt="logo" />
+        <img class="w-10 sm:w-20 mr-3 object-contain" src=${svgLogo} alt="logo" />
         <h1 class="font-medium leading-tight text-2xl sm:text-4xl lg:text-5xl leading-none">Anonymous youtube playlist generator</h1>
       </div>
       <p class="mb-4">Create youtube playlists without a google account</p>
